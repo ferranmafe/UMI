fcb19084880d38b171602f93146a5d52311a6155
Daniel Martinez Bordes <dmartinezbordes@gmail.com>
2018-03-24 20:08:31 +0100

    CtrlLeapmotion and Hand class implemented

--- src/domain/CtrlLeapmotion.py
+++ src/domain/CtrlLeapmotion.py
@@ -1,9 +1,12 @@
-import sys, Leap, Hand
+import sys, Leap, Hand, math
+
+
 
 class CtrlLeapmotion:
     ctrlLeapmotion = None
     hands = {}
     actFrame = None
+    thresholdAngle = 50 * math.pi/180.0
 
     def __init__(self):
         self.ctrlLeapmotion = Leap.Controller()
@@ -14,8 +17,8 @@ class CtrlLeapmotion:
             pass
         while (not self.ctrlLeapmotion.frame().is_valid):
             pass
-        actFrame = self.ctrlLeapmotion.frame()
-        handlist = actFrame.hands
+        self.actFrame = self.ctrlLeapmotion.frame()
+        handlist = self.actFrame.hands
         for hand in handlist:
             id = hand.id
             height = self.getHandHeight(hand)
@@ -28,11 +31,46 @@ class CtrlLeapmotion:
         for hand in self.hands:
             print self.hands[hand].height
 
+    def updateState(self):
+        while (not self.ctrlLeapmotion.frame().is_valid):
+            pass
+        if (self.actFrame.id == self.ctrlLeapmotion.frame().id):
+            return
+        self.actFrame = self.ctrlLeapmotion.frame()
+        print("\nFrame ID: " + str(self.actFrame.id) + "\n")
+        handlist = self.actFrame.hands
+        for hand in handlist:
+            id = hand.id
+            for finger in hand.fingers:
+                previousState = self.hands[id].getFingerState(finger.type)
+                eventDetected = self.detectEvent(finger)
+                fingerState = self.getNewState(previousState, eventDetected)
+                self.hands[id].setFingerState(finger.type, fingerState)
+            self.hands[id].printHandStatus()
 
+    def detectEvent(self, finger):
+        fingerMetacarpal = finger.bone(Leap.Bone.TYPE_METACARPAL).direction
+        fingerPhalange = finger.bone(Leap.Bone.TYPE_PROXIMAL).direction
+        detectedAngle = fingerMetacarpal.angle_to(fingerPhalange)
+        return detectedAngle >= self.thresholdAngle
+
+    def getNewState(self, previousState, eventDetected):
+        if (eventDetected and (previousState == Hand.finger_state.HELD or previousState == Hand.finger_state.TRIGGER)):
+            return Hand.finger_state.HELD
+        elif (eventDetected and previousState == Hand.finger_state.IDLE):
+            return Hand.finger_state.TRIGGER
+        return Hand.finger_state.IDLE
+
+    def degToRad(self, deg):
+        return deg * math.pi / 180.0
+
+    def radToDeg(self, rad):
+        return rad * 180.0 / math.pi
 
 def main():
     c = CtrlLeapmotion()
-    c.printAttributes()
+    while 1:
+        c.updateState()
 
 
 if __name__ == "__main__":
--- src/domain/Hand.py
+++ src/domain/Hand.py
@@ -1,3 +1,4 @@
+import Leap
 
 class hand_types:
     LEFT_HAND   = 0
@@ -14,9 +15,27 @@ class Hand:
     def __init__(self, hand_type, height):
         self.height     = height
         self.hand_type  = hand_type
-        self.finger_states = [finger_state.IDLE, finger_state.IDLE,
-                              finger_state.IDLE, finger_state.IDLE,
-                              finger_state.IDLE]
+        self.finger_states = {Leap.Finger.TYPE_THUMB : finger_state.IDLE,
+                              Leap.Finger.TYPE_INDEX : finger_state.IDLE,
+                              Leap.Finger.TYPE_MIDDLE : finger_state.IDLE,
+                              Leap.Finger.TYPE_RING : finger_state.IDLE,
+                              Leap.Finger.TYPE_PINKY : finger_state.IDLE}
+
+    def setFingerState(self, fingerType, fingerState):
+        self.finger_states[fingerType] = fingerState
+
+    def getFingerState(self, fingerType):
+        return self.finger_states[fingerType]
+
+    def printHandStatus(self):
+        print("Hand height: " + str(self.height) + "\n")
+        if self.hand_type :
+            print("Hand type: Right\n")
+        else:
+            print("Hand type: Left\n")
+        for f in self.finger_states:
+            print("\t" + str(f) + " -> " + str(self.finger_states[f]) + "\n")
+
 
 def main():
     x = Hand(1, 234.3)
