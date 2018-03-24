fff2e444a21145e096d3496abf15f76d605b402c
Adrian Lozano Navarro <adrian.lozano.navarro@est.fib.upc.edu>
2018-03-25 00:51:08 +0100

    Cosicas del mac petao

--- src/presentation/presentationController.py
+++ src/presentation/presentationController.py
@@ -4,20 +4,21 @@ import mainView
 class PresentationController(tk.Tk):
     def __init__(self):
       tk.Tk.__init__(self)
-      self.container = tk.Frame(self)
-      self.container.pack(side="top", fill="both", expand=True)
-      self._frame = mainView.MainView(master=self.container, controller=self)
       tk.Tk.minsize(self, 800, 600)
       tk.Tk.maxsize(self, 800, 600)
+      self.container = tk.Frame(self)
+      self.container.pack(side="top", fill="both", expand=1)
+      self._frame = mainView.MainView(master=self.container, controller=self)
 
     def switch_frame(self, frame_class):
-      """Destroys current frame and replaces it with a new one."""
       new_frame = frame_class(master=self.container, controller=self)
       self._frame.destroy()
       self._frame = new_frame
 
     def main_node_thread(self):
-      print('asd')
+      #llamar al dominio!
+      #obtener vector a imprimir!
+      None
 
 
 if __name__ == "__main__":
