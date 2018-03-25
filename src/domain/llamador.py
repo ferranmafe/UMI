119c775eb7b03e523bab53af84aec15d433233d0
Daniel Martinez Bordes <dmartinezbordes@gmail.com>
2018-03-25 01:34:04 +0100

    CtrlLeapmotion and Hand class implemented

--- src/domain/llamador.py
+++ src/domain/llamador.py
@@ -1,24 +1,16 @@
 import playNotes
 
-pn = playNotes.PlayNotes()
-pn.inicializar()
 
-pn.tocar(['la'])
-pn.tocar(['re'])
-pn.tocar(['fa'])
-pn.tocar(['la'])
-pn.tocar(['re'])
 
-pn.tocar(['fa'])
-pn.tocar(['la'])
-pn.tocar(['do*'])
-pn.tocar(['si'])
-pn.tocar(['sol'])
-pn.tocar(['fa'])
-pn.tocar(['sol'])
-pn.tocar(['la'])
+def main():
+    pn = playNotes.PlayNotes()
+    pn.inicializar()
 
-pn.tocar(['re'])
-pn.tocar(['do'])
-pn.tocar(['mi'])
-pn.tocar(['re'])
+    pn.tocar(['la','re','fa'])
+    pn.tocar(['mi'])
+    pn.tocar(['mi'])
+    pn.tocar(['mi'])
+    pn.tocar(['mi'])
+    while 1:
+        pass
+main()
--- src/domain/playNotes.py
+++ src/domain/playNotes.py
@@ -1,33 +1,22 @@
 import pygame
-import time
-class PlayNotes:
-    def __init__(self):
-        self.canales = []
-        self.sounds = {}
 
-    def inicializar(self):
+class SoundPlayer:
+    channels = []
+    sounds = []
+
+    def initializePiano(self):
         pygame.init()
-        canales = [0 for i in range(8)]
-        for i in range(8):
-            canales[i] = pygame.mixer.Channel(i)
+        channels = [0 for i in range(24)]
+        for i in range(24):
+            channels[i] = pygame.mixer.Channel(i)
 
-        sounds = {}
-        sounds['do'] = pygame.mixer.Sound("./sonidos/do.wav")
-        sounds['re'] = pygame.mixer.Sound("./sonidos/re.wav")
-        sounds['mi'] = pygame.mixer.Sound("./sonidos/mi.wav")
-        sounds['fa'] = pygame.mixer.Sound("./sonidos/fa.wav")
-        sounds['sol']= pygame.mixer.Sound("./sonidos/sol.wav")
-        sounds['la'] = pygame.mixer.Sound("./sonidos/la.wav")
-        sounds['si'] = pygame.mixer.Sound("./sonidos/si.wav")
-        sounds['do*'] = pygame.mixer.Sound("./sonidos/do'.wav")
-        self.canales = canales
+        sounds = [0 for i in range(24)]
+        for i in range(24):
+            sounds[i] = pygame.mixer.Sound("./sonidos/" + str(i) + ".wav")
+        self.channels = channels
         self.sounds = sounds
 
-
-    def tocar(self, notas):
-        j = 0
-        for i in range(len(notas)):
-            if notas[i] in self.sounds:
-                self.canales[j].queue(self.sounds[notas[i]])
-                j = j+1
-        time.sleep(0.3)
+    def playSounds(self, notas):
+        for i in len(notas):
+            if notas[i]:
+                self.channels[i].play(self.sounds[i])
